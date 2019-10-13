5e1c896c541f6580c6cfdd485554a1150e482228
galsim <44705564+galsim@users.noreply.github.com>
2019-10-13 13:14:36 +0300

    lesson 16

--- src/store/category.js
+++ src/store/category.js
@@ -15,8 +15,18 @@ export default {
         async fetchCategories({commit, dispatch}) {
             try {
                 const uid = await dispatch('getUid')
-                const category = (await firebase.database().ref(`/users/${uid}/categories`).once('value')).val() || {}
-                return Object.keys(category).map(key => ({...category[key], id: key}))
+                const categories = (await firebase.database().ref(`/users/${uid}/categories`).once('value')).val() || {}
+                return Object.keys(categories).map(key => ({...categories[key], id: key}))
+            } catch(e) {
+                commit('setError', e)
+                throw e 
+            }
+        },
+        async fetchCategoryById({commit, dispatch}, id) {
+            try {
+                const uid = await dispatch('getUid')
+                const category = (await firebase.database().ref(`/users/${uid}/categories`).child(id).once('value')).val() || {}
+                return {...category, id }
             } catch(e) {
                 commit('setError', e)
                 throw e 
