f9f6062fbe7db5ebc1c18bd2cd12cfbac68e74f4
galsim <44705564+galsim@users.noreply.github.com>
2019-10-13 16:42:05 +0300

    lesson 19

--- src/main.js
+++ src/main.js
@@ -5,6 +5,7 @@ import router from './router'
 import store from './store'
 import dateFilter from '@/filters/date.filter'
 import currencyFilter from '@/filters/currency.filter'
+import localizeFilter from '@/filters/localize.filter'
 import tooltipDirective from '@/directives/tooltip.directives.js'
 import messagePlugin from '@/utils/message.plugin'
 import Loader from '@/components/app/Loader'
@@ -23,6 +24,7 @@ Vue.use(Vuelidate)
 Vue.directive('tooltip',tooltipDirective)
 Vue.filter('date', dateFilter)
 Vue.filter('currency', currencyFilter)
+Vue.filter('localize', localizeFilter)
 Vue.component('Loader', Loader)
 Vue.component('Paginate', Paginate)
 
