3df3e84c7698834449d0acde2f265614d08488a1
galsim <44705564+galsim@users.noreply.github.com>
2019-10-13 11:59:10 +0300

    lesson 14 finish

--- src/router.js
+++ src/router.js
@@ -1,5 +1,6 @@
 import Vue from 'vue'
 import Router from 'vue-router'
+import firebase from 'firebase/app'
 
 Vue.use(Router)
 
@@ -65,7 +66,14 @@ const router = new Router({
 })
 
 router.beforeEach((to, from, next) => {
+  const currentUser = firebase.auth().currentUser
+  const requireAuth = to.matched.some(record => record.meta.auth) 
 
+  if (requireAuth && !currentUser) {
+    next('/login?message=login')
+  } else {
+    next() 
+  }
 })
 
 export default router
