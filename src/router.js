4a3987552c6c0f36c9cd92b4d58bd227a54feeab
maksim <golubev.mak.ser@gmail.com>
2019-10-13 11:38:06 +0300

    lesson 14

--- src/router.js
+++ src/router.js
@@ -3,7 +3,7 @@ import Router from 'vue-router'
 
 Vue.use(Router)
 
-export default new Router({
+const router = new Router({
   mode: 'history',
   base: process.env.BASE_URL,
   routes: [
@@ -22,44 +22,50 @@ export default new Router({
     {
       path: '/',
       name: 'home',
-      meta: {layout: 'main'},
+      meta: {layout: 'main', auth: true},
       component: () => import('./views/Home.vue')
     },
     {
       path: '/categories',
       name: 'categories',
-      meta: {layout: 'main'},
+      meta: {layout: 'main', auth: true},
       component: () => import('./views/Categories.vue')
     },
     {
       path: '/detail/:id',
       name: 'detail',
-      meta: {layout: 'main'},
+      meta: {layout: 'main', auth: true},
       component: () => import('./views/Detail.vue')
     },
     {
       path: '/history',
       name: 'history',
-      meta: {layout: 'main'},
+      meta: {layout: 'main', auth: true},
       component: () => import('./views/History.vue')
     },
     {
       path: '/planning',
       name: 'planning',
-      meta: {layout: 'main'},
+      meta: {layout: 'main', auth: true},
       component: () => import('./views/Planning.vue')
     },
     {
       path: '/profile',
       name: 'profile',
-      meta: {layout: 'main'},
+      meta: {layout: 'main', auth: true},
       component: () => import('./views/Profile.vue')
     },
     {
       path: '/record',
       name: 'record',
-      meta: {layout: 'main'},
+      meta: {layout: 'main', auth: true},
       component: () => import('./views/Record.vue')
     }
   ]
 })
+
+router.beforeEach((to, from, next) => {
+
+})
+
+export default router
